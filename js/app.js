7ca6ee7524f0e8c691a413ad5bd800ff9c72ba2c
Stanley Tsao <stsao002@gmail.com>
2016-12-15 13:37:56 -0800

    Create dropdown menu with states to sort by

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-$.getJSON('http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz19g5u4q5laj_5taq0&address=8144+Palm+View+Lane&citystatezip=Riverside%2C+CA&rentzestimate=true');
+
+
+$.ajax({
+	url: "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=wikiCallback&limit=10&search=basketball",
+	type: 'POST',
+	dataType: "jsonp",
+	success: function( response ) {
+	    
+	    var title = response[1][0];
+	    var link = response[3][0];
+	    var result = [];
+	    
+console.log(title + ' ' + link);
+
+	    // for (var i = 0; i < articleTitle.length; i++){
+	    //     var title = articleTitle[i];
+	    //     var link = articleLink[i];
+	    //     result.push('<li><a href="'+link+'"target="_blank">'+title+'</a></li>');
+	    // }
+
+	}
+});
