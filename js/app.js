b129bf0ca246a848393513edd44a60707b29bee1
Stanley Tsao <stsao002@gmail.com>
2016-12-16 16:45:54 -0800

    Feat: dropdown menu filters schools

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
+function viewModel() {
+    var self = this;
+
+ 	schoolOptions = ko.observableArray(allSchools);
+
+ 	selectedState = ko.observable('East Coast');
+
+ 	self.filterSchools = function() {
+
+ 		var toggleSchools = allSchools.forEach(function(obj, key) {
+ 			if (obj.state !== selectedState()) {
+				$('#schoolList')["0"].children[key].className = 'hide';
+ 			} else {
+ 				$('#schoolList')["0"].children[key].className = 'show';
+ 			}
+ 		});
+
+ 		// var toggleSchools = allSchools.forEach(function(obj, key) {
+ 		// 	if (obj.state !== selectedState()) {
+			// 	$('#schoolList')["0"].children[key].className = 'hide';
+ 		// 	} else {
+ 		// 		$('#schoolList')["0"].children[key].className = 'show';
+ 		// 	}
+ 		// });
+
+ 	}
+
+};
+ko.applyBindings(new viewModel());
 
 
+
+
+// Wikipedia API
 $.ajax({
 	url: "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=wikiCallback&limit=10&search=basketball",
 	type: 'POST',
@@ -9,8 +41,6 @@ $.ajax({
 	    var title = response[1][0];
 	    var link = response[3][0];
 	    var result = [];
-	    
-console.log(title + ' ' + link);
 
 	    // for (var i = 0; i < articleTitle.length; i++){
 	    //     var title = articleTitle[i];
--- js/model/schools.js
+++ js/model/schools.js
@@ -1,4 +1,4 @@
-var states = [
+var states = ko.observableArray([
   'Illinois',
   'Indiana',
   'Ohio',
@@ -10,22 +10,69 @@ var states = [
   'Washington D.C.',
   'Virginia',
   'North Carolina',
-];
+]);
 
-var schools = [
+var allSchools = [
   {
-    title : 'Rutgers',
+    title : 'Rutgers University',
     location : {lat: 40.5014869, lng: -74.44705359999999},
-    state : 'NJ'
+    state : 'New Jersey'
   },
   {
-    title : 'George Mason',
+    title : 'George Mason University',
     location : {lat: 38.8345098, lng: -77.31111559999999},
-    state : 'VA'
-  }
+    state : 'Virginia'
+  },
+  {
+    title : 'Georgetown University',
+    location : {lat: 38.9076708, lng: -77.0715752},
+    state : 'Washington D.C.'
+  },
+  {
+    title : 'George Washington University',
+    location : {lat: 38.9009637, lng: -77.0476606},
+    state : 'Washington D.C.'
+  },
+  {
+    title : 'Carnegie Mellon University',
+    location : {lat: 40.4434658, lng: -79.9434567},
+    state : 'Pennsylvania'
+  },
+  {
+    title : 'Northwestern University',
+    location : {lat: 42.0505569, lng: -87.67972809999999},
+    state : 'Illinois'
+  },
+  {
+    title : 'University of Chicago',
+    location : {lat: 41.7895697, lng: -87.60091480000001},
+    state : 'Illinois'
+  },
+  {
+    title : 'Princeton University',
+    location : {lat: 40.35334290000001, lng: -74.6160467},
+    state : 'New Jersey'
+  },
+  {
+    title : 'Purdue University',
+    location : {lat: 40.4282261, lng: -86.9144194},
+    state : 'Indiana'
+  },
+  {
+    title : 'University of Pittsburgh',
+    location : {lat: 40.4443282, lng: -79.95315479999999},
+    state : 'Pennsylvania'
+  },
   // {
   //   title : '',
   //   location : {lat: , lng: },
   //   state : ''
-  // }
+  // },
+  // {
+  //   title : '',
+  //   location : {lat: , lng: },
+  //   state : ''
+  // },
 ];
+
+
